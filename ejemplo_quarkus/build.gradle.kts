4d1ed4a198ff14252ddef3546a6e47aef2dac740
Alfred <anthonysub300@gmail.com>
2024-12-01 16:47:24 -0500

    Uso de Panache

--- ejemplo_quarkus/build.gradle.kts
+++ ejemplo_quarkus/build.gradle.kts
@@ -27,7 +27,9 @@ dependencies {
     implementation("io.quarkus:quarkus-rest")
     implementation("io.quarkus:quarkus-rest-jsonb")
     //JPA
-    implementation("io.quarkus:quarkus-hibernate-orm")
+    implementation("io.quarkus:quarkus-hibernate-orm-panache")
+    implementation("io.quarkus:quarkus-jdbc-postgresql")
+    implementation("org.postgresql:postgresql:42.7.4")
 
 }
 
